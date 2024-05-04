1d711a4fb95355199bd8c9dd4e31d9a1e41b0705
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-05-05 06:47:38 +0700

    START = newNode

--- 0050__Doublelinkedlist.cpp
+++ 0050__Doublelinkedlist.cpp
@@ -45,5 +45,26 @@ void addNode()
 			current = current->next;
 		}
 
+		newNode->next = current;
+		newNode->prev = previous;
 
+		if (current != NULL)
+		{
+			current->prev = newNode;
+		}
+		if (previous != NULL)
+		{
+			previous->next = newNode;
+		}
+		else
+		{
+			START = newNode;
+		}
+	}
 }
+
+
+
+		
+
+
