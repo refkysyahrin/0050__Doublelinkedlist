e69061002d0b592a6b18a354a0bea198c3244248
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-05-05 06:58:25 +0700

    if null

--- 0050__Doublelinkedlist.cpp
+++ 0050__Doublelinkedlist.cpp
@@ -102,3 +102,14 @@ void deleteNode()
 		cout << "\033[31mThe record with roll number " << rollNo << " not found\033[0m" << endl;
 		return;
 	}
+
+	if (current == START)
+	{
+		START = START->next;
+		if (START != NULL)
+		{
+			START->prev = NULL;
+		}
+	}
+	else
+	{
